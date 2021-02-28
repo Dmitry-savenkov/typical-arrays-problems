4a2d6c91cc75276e8d5ba90abd9f5984461f5c65
Dmitry_Savenkov <73306821+Dmitry-savenkov@users.noreply.github.com>
2021-02-28 13:03:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,34 +1,14 @@
-exports.min = function min(array) {
-    if (array.length === 0 || array === undefined) {
-        return 0;
-    }
-    let minNumber = array[0];
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] < minNum) {
-            minNumber = array[i];
-        }
-    }
-    return minNumber;
+exports.min = function min (array) {
+  if (!array || array.length === 0) return 0;
+  return Math.min(...array);
 }
 
-exports.max = function max(array) {
-    if (array.length === 0 || array === undefined) {
-        return 0;
-    }
-    let maxNumber = array[0];
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] > maxNum) {
-            maxNumber = array[i];
-        }
-    }
-    return maxNumber;
+exports.max = function max (array) {
+  if (!array || array.length === 0) return 0;
+  return Math.max(...array);;
 }
 
-exports.avg = function avg(array) {
-    if (array.length === 0 || array === undefined) {
-        return 0;
-    }
-    let len = array.length;
-    let sum = array.reduce((previousValue, currentValue) => previousValue + currentValue);
-    return (sum / len);
+exports.avg = function avg (array) {
+  if (!array || array.length === 0) return 0;
+  return (array.reduce((al, re) => al + re, 0) / array.length);
 }
